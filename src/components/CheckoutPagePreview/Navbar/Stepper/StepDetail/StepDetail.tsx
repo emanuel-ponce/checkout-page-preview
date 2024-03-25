963dcf29f94bb58aed52bf2b8983bc2ce2c3d036
emanuelnponce <emanuel@wisboo.com>
2024-03-24 21:32:34 -0300

    refactor: improved step detail implementation

--- src/components/CheckoutPagePreview/Navbar/Stepper/StepDetail/StepDetail.tsx
+++ src/components/CheckoutPagePreview/Navbar/Stepper/StepDetail/StepDetail.tsx
@@ -9,13 +9,13 @@ function StepDetail({}: StepDetailProps) {
   const dispatch = useAppDispatch();
   const currentStep = useAppSelector(selectCurrentStep);
   const totalSteps = useAppSelector(selectTotalSteps);
-  const texts = [null, 'Choose your use case', 'Customize', 'Try it out'];
+  const texts = ['Choose your use case', 'Customize', 'Try it out'];
   const [isOnHover, setIsOnHover] = useState(false);
   return (
     <div className={`min-w-[140px] ${currentStep > 1 ? 'cursor-pointer' : ''}`} onClick={() => currentStep > 1 && dispatch(beforeStep())} onMouseEnter={() => setIsOnHover(true)} onMouseLeave={() => setIsOnHover(false)}>
       <div className="flex items-center">
         {currentStep > 1 && <FiArrowLeft className="text-theme-iconGray mr-2" />}
-        <span className="font-medium">{currentStep === 1 || !isOnHover ? texts[currentStep] : 'Back'}</span>
+        <span className="font-medium">{currentStep === 1 || !isOnHover ? texts[currentStep - 1] : 'Back'}</span>
         {(currentStep === 1 || !isOnHover) && (
           <span className="text-xs ml-2">
             {currentStep} of {totalSteps}
