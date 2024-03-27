d7bcc890129ba7d72c5f939c257b69885769a560
emanuelnponce <emanuel@wisboo.com>
2024-03-26 21:52:55 -0300

    chore: updated scripts in package json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Inter } from "next/font/google";
-import CheckoutPagePreview from "@components/CheckoutPagePreview/CheckoutPagePreview";
-import GeneralLayout from "@layouts/GeneralLayout";
-
-const inter = Inter({ subsets: ["latin"] });
+import CheckoutPagePreview from '@components/CheckoutPagePreview/CheckoutPagePreview';
+import GeneralLayout from '@layouts/GeneralLayout';
 
 export default function Home() {
   return (
     <GeneralLayout>
-      <CheckoutPagePreview/>
+      <CheckoutPagePreview />
     </GeneralLayout>
   );
 }
