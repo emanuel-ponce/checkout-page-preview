d94a817d8abeb87ed418f051a4ddf1c6725f7a82
emanuelnponce <emanuel@wisboo.com>
2024-03-26 23:36:37 -0300

    chore: added CORS

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,19 @@ export default {
   images: {
     domains: ['images.unsplash.com', 'www.cicloplast.com', 'static-00.iconduck.com']
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' }
+        ]
+      }
+    ];
+  },
   webpack: (config, options) => {
     config.plugins.push(
       new NextFederationPlugin({
